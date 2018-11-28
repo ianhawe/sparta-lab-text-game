fe97a161993531d266309d923ebee52b3f455364
Ian <ianhawe24@hotmail.co.uk>
2018-11-28 02:30:52 +0000

    Birthmark boy text-based game

---
+++ main.js
@@ -0,0 +1,23 @@
+let b = 0;
+while (b == 0) {
+    console.log("Welcome to Dungeons of Dragons!");
+
+    const playerInventory = [];
+    const playerName = console.log("What is your name?");
+    // Player needs to enter a  name
+    console.log("Welcome " + printName + " are you prepared to sit here for the ultimate text game!");
+
+
+    const printName = String => { console.log(playerName) }
+
+    //    die function() or 
+
+
+}
+// const printName = String => {
+
+//     for (int i = 0; i < playerArray; i++) {
+//         console.log(playerArray[0]);
+//     };
+
+// }
