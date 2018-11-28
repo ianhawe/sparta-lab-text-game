47f502ca66a95010ef1cce87ea1510d85484c04c
Ian <ianhawe24@hotmail.co.uk>
2018-11-28 10:25:42 +0000

    comments everywhere

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ alert("You're born with a weird birthmark on your face. You never knew what the 
 let playerName = prompt(" A stranger approaches you. \n  Stranger: Child. What is your name?");
 // const playerItems = [sword, shield, apple, bread, water]
 // playerItems 
-let choice1 = prompt(playerName + " Why are you out here alone? \n 1. Ignore him \n 2. Tell him you're looking for food \n 3. Tell him he should mind his own business. \n 4. Tell him you don't talk to strangers. \n Please choose 1-4");
+let choice1 = prompt(playerName + " why are you out here alone? \n 1. Ignore him \n 2. Tell him you're looking for food \n 3. Tell him he should mind his own business. \n 4. Tell him you don't talk to strangers. \n Please choose 1-4");
 
 //                                                                                               Lore
 //ignore him
@@ -39,7 +39,9 @@ if (choice1 === "1") {
 //tell him you're looking for food
 else if (choice1 === "2") {
     let choice21 = prompt("*Laugh* \n I can teach you to find food. Would you like to learn? \n ");
-    if (choice21) { }
+    if (choice21) {
+        alert("The Stranger has deleted you. Goodbye " + playerName + "." + " \n Better luck next time!");
+    }
 
 }
 //tell him he should mind his own business
@@ -62,7 +64,7 @@ function pathJoin1121Pathjoin1122(String) {
     //                                                                                               Lore
     //                                                                                          Mountain Mama    
     //Story
-    alert("The monk gesters to his right, a portal appears. \n Stranger:'After you.' \n You enter the portal wondering what awaits you on the other side. \n You emerge from the portal and arrive in a lustrious green forest and view your surroundings.");
+    alert("The monk gesturs to his right, a portal appears. \n Stranger:'After you.' \n You enter the portal wondering what awaits you on the other side. \n You emerge from the portal and arrive in a lustrious green forest and view your surroundings.");
 
     //Starting point
     let choice2 = prompt("Stranger: What's that over there? \n *Stranger points* \n *You notice a mountain ahead of you* \n Stranger: Do you remember the way? \n *You nod your head* \n Stranger: You lead, I'll follow. \n Where do you want to move to? \n 1. Forward \n 2. Left \n 3. Right");
@@ -96,11 +98,13 @@ function pathJoin1121Pathjoin1122(String) {
     }
     //left
     else if (choice2 == "2") {
-        let choice21 = prompt("*You hop over a fallen log* \n You make your way forward deeper into the forest \n ");
+        alert("The Stranger has deleted you. Goodbye " + playerName + "." + " \n Better luck next time!");
+        //  let choice21 = prompt("*You hop over a fallen log* \n You make your way forward deeper into the forest \n ");
     }
     //right
     else if (choice2 == "3") {
-        let choice21 = prompt("*You hop over a fallen log* \n You make your way forward deeper into the forest \n ");
+        alert("The Stranger has deleted you. Goodbye " + playerName + "." + " \n Better luck next time!");
+        // let choice21 = prompt("*You hop over a fallen log* \n You make your way forward deeper into the forest \n ");
     }
 }
 
@@ -164,65 +168,65 @@ function portalPath(String) {
     alert("The stranger waves his hand and you are consumed by a portal.");
     alert("You realise you're in the centre of a room, with no exit, alone.\n Strange Voice: Here begins your trial, attempt to escape this room. Fail to do so and you will be trapped in this realm");
     let directionOfPlayer = centerRoom(String);
-
-    if (directionOfPlayer == "1") {
-
-    }
-    else if (directionOfPlayer == "2") {
-
-    }
-    else if (directionOfPlayer == "3") {
-
-    }
-    else if (directionOfPlayer == "4") {
-
-    }
-
-
 }
+//     if (directionOfPlayer == "1") {
+
+//     }
+//     else if (directionOfPlayer == "2") {
+
+//     }
+//     else if (directionOfPlayer == "3") {
+
+//     }
+//     else if (directionOfPlayer == "4") {
+
+//     }
 
 
-function centerRoom(String) {
-    let String = prompt("Where do you want to go? \n 1.Up \n 2.Right \n 3.Down \n 4. Left");
-}
-
-let top = String => {
-    let input = prompt("Adjacent to you is a wall! \n Do you want to touch the wall? \n 1. Touch the wall \n 2. Go back \n 3. Go right 4. Go left");
-    if (input = 1) {
-        touchWallInCorrect();
-        let input = prompt("Adjacent to you is a wall! \n Do you want to touch the wall? \n 1. Go back \n 2. Go right 3. Go left");
-
-    }
-    else if (input = 2) {
-        centerRoom();
-    }
-    else if (input = 3) {
-        corners();
-
-    }
-    else if (input = 4) {
-        corners();
-    }
-
-}
-function right() { }
+// }
 
 
+// function centerRoom(String) {
+//     let String = prompt("Where do you want to go? \n 1.Up \n 2.Right \n 3.Down \n 4. Left");
+// }
 
-function corners(string) {
-    string = alert("You have reached the corner of the room \n where would you like to go? \n 1. Go back. \n 2. Go forward \n 3. Touch the wall");
-}
+// let top = String => {
+//     let input = prompt("Adjacent to you is a wall! \n Do you want to touch the wall? \n 1. Touch the wall \n 2. Go back \n 3. Go right 4. Go left");
+//     if (input = 1) {
+//         touchWallInCorrect();
+//         let input = prompt("Adjacent to you is a wall! \n Do you want to touch the wall? \n 1. Go back \n 2. Go right 3. Go left");
 
-function bottom() { }
+//     }
+//     else if (input = 2) {
+//         centerRoom();
+//     }
+//     else if (input = 3) {
+//         corners();
+
+//     }
+//     else if (input = 4) {
+//         corners();
+//     }
+
+// }
+// function right() { }
 
 
-function touchWallCorrect(String) {
-    alert("You hear a creaking sound!");
 
-}
-function touchWallInCorrect() {
-    alert("Nothing Happened.");
-}
+// function corners(string) {
+//     string = alert("You have reached the corner of the room \n where would you like to go? \n 1. Go back. \n 2. Go forward \n 3. Touch the wall");
+// }
+
+// function bottom() { }
+
+
+// function touchWallCorrect(String) {
+//     alert("You hear a creaking sound!");
+
+// }
+// function touchWallInCorrect() {
+//     alert("Nothing Happened.");
+// }
 
 
 //    - - -
